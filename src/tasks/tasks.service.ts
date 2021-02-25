518772c205ea2daa02d5fc769b78cb37267c2586
Ynah Pantig <me@ynahpantig.com>
2021-02-25 03:11:19 -0800

    Refactor filtering controller and service

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -23,24 +23,9 @@ export class TasksService {
     return found;
   }
 
-  // private tasks: Task[] = [];
-  // getAllTasks(): Task[] {
-  //   return this.tasks;
-  // }
-  // getFilteredTasks(filterDto: GetTasksFilterDto): Task[] {
-  //   const { status, search } = filterDto;
-  //   let tasks = this.getAllTasks();
-  //   if (status) {
-  //     tasks = tasks.filter((task) => task.status === status);
-  //   }
-  //   if (search) {
-  //     tasks = tasks.filter(
-  //       (task) =>
-  //         task.title.includes(search) || task.description.includes(search),
-  //     );
-  //   }
-  //   return tasks;
-  // }
+  async getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
+    return this.taskRepository.getTasks(filterDto);
+  }
 
   async deleteTaskById(id: number): Promise<void> {
     const result = await this.taskRepository.delete(id);
