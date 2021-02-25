fbf10fe5fa799c6d6b9a7e09322fe4a0dff3c653
Ynah Pantig <me@ynahpantig.com>
2021-02-25 01:48:32 -0800

    Add TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksModule } from './tasks/tasks.module';
+import { typeOrmConfig } from './config/typeorm.config';
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    TasksModule,
+    TypeOrmModule.forRoot({
+      ...typeOrmConfig,
+      autoLoadEntities: true,
+    }),
+  ],
 })
 export class AppModule {}
